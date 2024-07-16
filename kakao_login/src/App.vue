ab24b75c6a8232e612b8f9f2757af998d2e76adb
Quokka <wotjr1142@gmail.com>
2024-07-16 18:08:48 +0900

    0716

---
+++ kakao_login/src/App.vue
@@ -0,0 +1,13 @@
+8<script  setup>
+  import kakaoLogin from"@/components/kakaoLogin.vue";
+</script>
+
+<template>
+<header>
+
+</header>
+
+<main>
+<kakaoLogin/>
+</main>
+</template>
---
+++ kakao_login/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+window.Kakao.init("86d754d29d91510b5e8fbd8a03331c33");
